3db35abe8ada065ced13da12c383f531d106b295
dervism <dervis4@gmail.com>
2019-07-30 16:18:06 +0200

    Refactor test class.

--- src/test/java/no/dervis/gts/TwitterAPITest.java
+++ src/test/java/no/dervis/gts/TwitterAPITest.java
@@ -3,20 +3,18 @@ package no.dervis.gts;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 import twitter4j.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -37,7 +35,10 @@ class TwitterAPITest {
 
         TwitterStats stats = new TwitterStats(new TwitterAPI(twitterMock));
 
-        assertEquals("test1", stats.showUser("test").getScreenName());
+        User test = stats.showUser("test");
+
+        assertNotNull(test);
+        assertEquals("test1", test.getScreenName());
     }
 
     @Test
@@ -55,6 +56,25 @@ class TwitterAPITest {
     @Test
     void getFollowers() throws TwitterException {
 
+        TwitterStats stats = new TwitterStats(new TwitterAPI(setupPagableResponsListMock()));
+
+        List<User> followers = stats.getFollowers("test");
+
+        assertNotNull(followers);
+        assertEquals(4, followers.size());
+    }
+
+    @Test
+    void getFollowersList() throws TwitterException {
+
+        TwitterStats stats = new TwitterStats(new TwitterAPI(setupPagableResponsListMock()));
+
+        List<String> followersList = stats.getFollowersList("");
+        assertNotNull(followersList);
+        assertEquals(Arrays.asList("test1", "test2", "test3", "test4"), followersList);
+    }
+
+    private Twitter setupPagableResponsListMock() throws TwitterException {
         Function<AtomicInteger, Stream<User>> func = i -> Stream.generate(() -> makeTestUser(i.getAndIncrement())).limit(2);
 
         PagableResponseList page1 = mock(PagableResponseList.class);
@@ -69,11 +89,7 @@ class TwitterAPITest {
         when(twitterMock.getFollowersList(anyString(), eq(-1L), anyInt())).thenReturn(page1);
         when(twitterMock.getFollowersList(anyString(), eq(1L), anyInt())).thenReturn(page2);
 
-        TwitterStats stats = new TwitterStats(new TwitterAPI(twitterMock));
-
-        assertEquals(4, stats.getFollowers("test").size());
-
-        assertEquals(Arrays.asList("test1", "test2", "test3", "test4"), stats.getFollowersList(""));
+        return twitterMock;
     }
 
     private List<User> makeTestUsers(int n) {
